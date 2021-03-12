c5781b17affc8c68f7a74377f41bf7ae02d41362
hejun <hejun@megvii.com>
2021-03-12 13:36:11 +0800

    from http://www.xuyasong.com/\?p\=2049

--- vendor/github.com/opencontainers/runc/libcontainer/cgroups/fs/kmem.go
+++ vendor/github.com/opencontainers/runc/libcontainer/cgroups/fs/kmem.go
@@ -21,17 +21,17 @@ func EnableKernelMemoryAccounting(path string) error {
 	// Ensure that kernel memory is available in this kernel build. If it
 	// isn't, we just ignore it because EnableKernelMemoryAccounting is
 	// automatically called for all memory limits.
-	if !cgroups.PathExists(filepath.Join(path, cgroupKernelMemoryLimit)) {
-		return nil
-	}
+	// if !cgroups.PathExists(filepath.Join(path, cgroupKernelMemoryLimit)) {
+	// 	return nil
+	// }
 	// We have to limit the kernel memory here as it won't be accounted at all
 	// until a limit is set on the cgroup and limit cannot be set once the
 	// cgroup has children, or if there are already tasks in the cgroup.
-	for _, i := range []int64{1, -1} {
-		if err := setKernelMemory(path, i); err != nil {
-			return err
-		}
-	}
+	// for _, i := range []int64{1, -1} {
+	// 	if err := setKernelMemory(path, i); err != nil {
+	// 		return err
+	// 	}
+	// }
 	return nil
 }
 
